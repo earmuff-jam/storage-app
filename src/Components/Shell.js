50d7a4c5ab3df1d490728fe73271b66072e5b4ee
mohit paudyal <mohit.paudyal@gmail.com>
2022-09-30 07:42:10 -0500

    Updated MUIV5 conversion
    Decided to use muiv5 to support a lot of in house styling and that is fine for now. We will change it later if needed. Looks exactly like how it used to before.
    
    I also decided that we should add Autocomplete and start laying the
    visual aspect if anything.

--- src/Components/Shell.js
+++ src/Components/Shell.js
@@ -1,15 +1,41 @@
 import React from "react";
 import styled from "styled-components";
+import { Autocomplete, TextField } from "@mui/material";
 
 const Shell = () => {
-  return <ShellStyledDiv>INPUT FIELD IN INPUT FIELD</ShellStyledDiv>;
+  return (
+    <ShellStyledDiv>
+      <Autocomplete
+        id="item-locator-1"
+        options={genericHouseholdItems}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Items ..." />}
+      />
+      IN
+      <Autocomplete
+        id="item-locator-1"
+        options={genericHouseholdItems}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Items ..." />}
+      />
+    </ShellStyledDiv>
+  );
 };
 
-export const ShellStyledDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-`;
+export const ShellStyledDiv = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+});
+
+const genericHouseholdItems = [
+  { label: "Black Container", itemDepth: 0 },
+  { label: "Small tube end sprayer", itemDepth: 1 },
+  { label: "Fuel Canister", itemDepth: 1 },
+  { label: "Blue Container", itemDepth: 2 },
+  { label: "Dog Food - Chow Expert", itemDepth: 3 },
+  { label: "Dog poop bags", itemDepth: 3 },
+];
 
 export default Shell;
